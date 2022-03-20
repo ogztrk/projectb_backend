876a77ada1a083ff1dacc936960d911c7e3631b8
oğuz türk <ogitr16@gmail.com>
2022-03-20 18:56:26 +0300

    #controller ve service kontrolü.

---
+++ src/main/java/com/badibul/backend/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.badibul.backend.controller;
+
+import com.badibul.backend.entity.User;
+import com.badibul.backend.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public List<User> getAll(){
+        return  userService.getAll();
+    }
+}
---
+++ src/main/java/com/badibul/backend/service/UserService.java
@@ -0,0 +1,22 @@
+package com.badibul.backend.service;
+
+import com.badibul.backend.entity.User;
+import com.badibul.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService{
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAll(){
+
+        return  userRepository.findAll();
+
+    }
+}
